ba0ff66944c7057a71d341258332fa1216dc4dfe
hunterjules <160811187+hunterjules@users.noreply.github.com>
2024-09-29 18:01:46 -0700

    Update mark-sweep.py

--- mark-sweep.py
+++ mark-sweep.py
@@ -1,4 +1,5 @@
-# Mark-Sweep garbage collection simulation for CPSC 3400
+# Mark-Sweep garbage collection simulation for CPSC 3400 at Seattle University 
+# Assignment topic credit to Professor Beatriz Diaz Acosta, code written my me.
 # Designed for file where the first line contains n, the number of heap blocks
 #    and subsequent heap blocks are identified using the numbers 0 through n-1.
 #    Each subsequent line contains an ordered pair in one of the following forms:
